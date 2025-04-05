ecbb5baac05aa3221f9960041388a0f3ce963256
Taubek-29 <taubek.smagulov27@fizmat.kz>
2025-04-05 13:15:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-class Cube { 
-    constructor (game) { 
-        this. geometry = {
-            pieceCornerRadius: 0.12,
-            edgeCornerRoundness: 0.15,
-            edgeScale: 0.82, 
-            edgeDepth: 0.01
-        };
-
-        this.holder = new THREE.Object3D(); 
-        this.object = new THREE.Object3D(); 
-        this.animator = new THREE.Object3D();
-
-        this.holder.add(this.animator); 
-        this.animator.add(this.object);
-        
-        this.game.world.scene.add(this.holder);
-    }
-}
+document.getElementById("portfolio-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    
+    const name = document.getElementById("name").value;
+    sessionStorage.setItem("portfolioName", name);
+    window.location.href = "portfolio.html";
+});
